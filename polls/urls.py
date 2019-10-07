efa2bf37a1a73bc955c881f5d2eee35ddc9242cc
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-10-07 14:16:55 +0700

    basic route view

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello Rifqi")
+
+def detail(request, question_id):
+    return HttpResponse(f"You Look At The Question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You Look At The Results of Question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're Voting on Question {question_id}")
